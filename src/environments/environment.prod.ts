82614645829003ab85df3044845c2b8054c19abe
Lamont <matthew.lamont@travelport.com>
2017-07-18 11:24:59 -0600

    env tweaks

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  bindoApiUrl: 'http://www.whvapor.com',
+  bindoApiUrl: 'https://white-horse-vapor-denver.herokuapp.com',
   contactEmail: 'vapedenver@gmail.com',
   googleMapsApiKey: 'AIzaSyAygK8MzLci1lhhWGjNaIk64xBYZD6mZdI'
 };
